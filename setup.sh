e763cfe51e71ebe9ebf697ad7a6d00dac312c531
Abraão Silva <abraaojs.dev@gmail.com>
2018-11-12 18:14:23 -0200

    add jfrog artifactory

--- setup.sh
+++ setup.sh
@@ -13,5 +13,5 @@ docker-compose up -d
 ### Warning ###
 # if the Jenkins of the following permission error
 # - /var/jenkins_home/copy_reference_file.log’: Permission denied 
-# - Execute: chown -R 1000:1000 /jenkins-stack-tools
+# - Execute: chown -R 1000:1000 jenkins-stack-tools/
 
